37ff61cf97bbecec298dd6eedc27d769ca95aa6d
Anders Knudsen <knudsen879@hotmail.com>
2018-03-15 14:36:11 +0100

    tilføjet students til et array

--- Klasse.java
+++ Klasse.java
@@ -1,18 +1,35 @@
 import java.util.*;
-public  class Klasse2{
+public  class Klasse{
    public static void main(String[]args){
    
       /// proper 10 af class "Student" i et array, kalder det "stud"
-      Student[] stud = new Student[10]; 
+      Student[] stud = new Student[3]; 
       /// test af hvad det indeholder     
       System.out.println(Arrays.toString(stud));
-      /// test af hvad en "stud" indeholder
-      System.out.println(stud[0]);
+      /// instanciere hvert indeks af "stud"
+      stud[0] = new Student("Torben",8.7,12);
+      stud[1] = new Student("Iben",8.9,13);
+      // stud[2] bliver instancieret med en constructor
+      stud[2] = new Student();
+      /// test om det virker
+      System.out.println(Arrays.toString(stud));
       
       
-      /// setter name, grade og age på stud[0]
-      stud[0].setName("torben");
-      stud[0].setGrade(7.8);
-      stud[0].setAge(12);
+      /// setter name, grade og age på stud[2], væk fra constructor
+      stud[2].setStudent("BERTRAM",0.1,18);
+      System.out.println(stud[2]);
+      
+      /// nyt array "students". indeks er 15 stort.
+      Student[] students = new Student[15];
+      /// hvert indeks er nu blankt "null"
+      
+      /// hvert indeks bliver nu instancieret til default constructor values
+      for ( int i = 0; i<students.length; i++){
+         students[i] = new Student();
+      }
+      /// print af hver lille lort..
+      for (int i = 0; i<students.length; i++){
+         System.out.println("student "+(i+1)+" ser således ud\n"+students[i]);
+      }
    }
 }
