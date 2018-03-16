64318902a368ffa15f5be01c1576f4c8807053cd
Anders Knudsen <knudsen879@hotmail.com>
2018-03-16 10:41:10 +0100

    opdelt i direktiver

---
+++ car project/Tire.java
@@ -0,0 +1,15 @@
+public class Tire{
+   private String brand;
+   private int tommer;
+   
+   public Tire(String brand, int tommer){
+      this.brand = brand;
+      this.tommer = tommer;
+   }
+   
+   public void print(){
+      System.out.println("Tire:");
+      System.out.println("Tire brand: "+brand);
+      System.out.println("Tire inches: "+tommer);
+
+}
